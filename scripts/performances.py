481acd67652c3aa1be5ede436ebea684fcc8c664
Michal Pleskowicz <plaskacz.panda@gmail.com>
2019-10-28 16:14:22 +0100

    Fixed a problem with the performance function

--- scripts/performances.py
+++ scripts/performances.py
@@ -1,41 +1,44 @@
 import numpy as np 
 
 # takes as an argument pure, unlabeled Y and Y_predicted
-def performance(Y, Y_predicted):
+def performance(y_given, y_predicted):
     best_t = 0
     best_perf = 0
+    y_labeled = label_results(y_given, 0.5)
     for i in range(0, 100):
-        perf = np.sum(label_results(Y) == label_results(Y_predicted, threshold=i/100))/Y.shape[0]
+        perf = np.sum(np.equal(y_labeled,label_results(y_predicted, i/100)))/y_given.shape[0]
         if(perf>best_perf):
             best_t = i/100
             best_perf = perf
-    return best_t, best_perf    
+    return best_t, best_perf
 
-def evaluate_performance(Y, Y_predicted):
+def evaluate_performance(y_given, y_predicted):
     false_negatives = 0
     true_negatives = 0
     false_positives = 0
     true_positives = 0
-    for i in range(Y.shape[0]):
-        if(Y[i]==-1):
-            if(Y_predicted[i]==-1):
+    for i in range(y_given.shape[0]):
+        if(y_given[i]==-1):
+            if(y_predicted[i]==-1):
                 true_negatives += 1
             else:
                 false_positives += 1
         else:
-            if(Y_predicted[i]==-1):
+            if(y_predicted[i]==-1):
                 false_negatives += 1
             else:
                 true_positives += 1
-    #print('True positives: ' + str(true_positives))
-    #print('False positives: ' + str(false_positives))
-    #print('True negatives: ' + str(true_negatives))
-    #print('False negatives: ' + str(false_negatives))
+    print('True positives: ' + str(true_positives))
+    print('False positives: ' + str(false_positives))
+    print('True negatives: ' + str(true_negatives))
+    print('False negatives: ' + str(false_negatives))
     
-def label_results(Y_predicted, threshold=0.5):
+def label_results(y_predicted, threshold=0.5):
     f = lambda x: -1 if x<threshold else 1
     f_vec = np.vectorize(f)
-    return f_vec(Y_predicted) 
+    output = np.copy(y_predicted)
+    output = f_vec(output) 
+    return output
 
 def test_polynomial_performance(X, Y, max_degree=15, method='least_squares'):
     # tests the performance of different degree polynomials, using least_squares
